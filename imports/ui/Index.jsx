6120a1cce32222a206470b432d9b491183643ff0
Mike Seid <mbseid@gmail.com>
2022-08-10 10:47:34 -0400

    Adding index page

--- imports/ui/Index.jsx
+++ imports/ui/Index.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useTracker } from 'meteor/react-meteor-data';
+
 // @mui
-import { useTheme } from '@mui/material/styles';
 import { Grid, Container, Typography } from '@mui/material';
 // components
 import Page from './components/Page';
-import Iconify from './components/Iconify';
 import AppWidgetSummary from './dashboard/AppWidgetSummary';
+import { Links } from '/imports/api/links';
+import { People } from '/imports/api/people';
 
 export const Index = () => {
-  const theme = useTheme();
+  const navigate = useNavigate()
+
+  const linkCount = useTracker(() => Links.find().count(),[]);
+  const peopleCount = useTracker(() => People.find().count(),[]);
 
   return (
     <Page title="Dashboard">
@@ -20,13 +25,13 @@ export const Index = () => {
 
         <Grid container spacing={3}>
           <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Link Count" total={714000} icon={'ant-design:link-outlined'} />
+            <AppWidgetSummary title="Link Count" total={linkCount} icon={'ant-design:link-outlined'} onClick={() => navigate('/links')}/>
           </Grid>
 
           <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Tag Count" total={1352831} color="info" icon={'ant-design:tag-filled'} />
+            <AppWidgetSummary title="People" total={peopleCount} color="info" icon={'ant-design:user-outlined'} onClick={() => navigate('/people')}/>
           </Grid>
-
+          {/*
           <Grid item xs={12} sm={6} md={3}>
             <AppWidgetSummary title="Item Orders" total={1723315} color="warning" icon={'ant-design:windows-filled'} />
           </Grid>
@@ -34,6 +39,7 @@ export const Index = () => {
           <Grid item xs={12} sm={6} md={3}>
             <AppWidgetSummary title="Bug Reports" total={234} color="error" icon={'ant-design:bug-filled'} />
           </Grid>
+          */}
         </Grid>
       </Container>
     </Page>
