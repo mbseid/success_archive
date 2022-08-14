9425d890641c3f6c4cb5442cea85dbb9e48c01f9
Mike Seid <mseid@spotify.com>
2022-08-13 22:04:07 -0400

    Adding in notes of the project

--- imports/ui/projects/view.jsx
+++ imports/ui/projects/view.jsx
@@ -37,7 +37,7 @@ import remarkBreaks from 'remark-breaks'
 ProfileCover.propTypes = {
 };
 
-function ProfileCover({ project }) {
+function ProfileCover({ project, completeClick }) {
   const { name, due } = project;
 
   return (
@@ -46,69 +46,20 @@ function ProfileCover({ project }) {
         ml: { md: 3 },
         mt: { xs: 1, md: 0 },
         textAlign: { xs: 'center', md: 'left' },
+        display: 'flex',
+        justifyContent: 'space-between'
       }}
     >
       <Stack>
         <Typography variant="h4">{name}</Typography>
         <Typography sx={{ opacity: 0.72 }}>{`Due: ${due.toDateString()}`}</Typography>
       </Stack>
+      <Box>
+          <Button variant="contained" color="success">Complete</Button>
+      </Box>
     </Box>
 
   );
-}
-
-function AddNoteForm({onSubmit, onCancel}){
-  const [note, setNote] = useState('')
-  const [date, setDate] = useState(new Date())
-  const submit = () => {
-    const log = {
-      note,
-      date
-    }
-    onSubmit(log)
-  }
-  return (
-    <Card sx={{ p: 3 }}>
-      <Stack spacing={3}>
-        <TextField
-          multiline
-          fullWidth
-          minRows={4}
-          placeholder="Record what you are discussing"
-          value={note}
-          onChange={(e) => setNote(e.target.value)}
-          sx={{
-            '& fieldset': {
-              borderWidth: `1px !important`,
-              borderColor: (theme) => `${theme.palette.grey[500_32]} !important`,
-            },
-          }}
-        />
-
-        <DesktopDatePicker
-          label="Date"
-          inputFormat="MM/dd/yyyy"
-          value={date}
-          onChange={(newDate) => setDate(newDate)}
-          renderInput={(params) => <TextField {...params} />}
-        />
-      </Stack>
-
-      <Box
-        sx={{
-          mt: 3,
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'end',
-          flexDirection: 'row-reverse',
-          columnGap: '2rem'
-        }}
-      >
-        <Button variant="contained" onClick={submit}>Save</Button>
-        <Button variant="outlined" color="error" onClick={onCancel}>Cancel</Button>
-      </Box>
-    </Card>
-  )
 }
 
 const MarkdownBox = styled(Box)({
@@ -121,7 +72,8 @@ const MarkdownBox = styled(Box)({
 });
 
 export default function ViewProject(){
-  const [addNote, setAddNote] = useState(false)
+  const [editNote, setEditNotes] = useState(false)
+  const [notes, setNotes] = useState()
 
   const params = useParams();
   const navigate = useNavigate();
@@ -130,6 +82,21 @@ export default function ViewProject(){
     return Projects.findOne({_id: params.id})
   });
 
+  const editNotes = () => {
+      setNotes(project.notes)
+      setEditNotes(true)
+  }
+
+  const saveNotes = () => {
+      Projects.update(project._id, {
+        $set: {
+            notes: notes
+        }
+      })
+      setEditNotes(false)
+
+  }
+
   return (
     <Page title="People">
       <Container>
@@ -137,19 +104,31 @@ export default function ViewProject(){
           {project ?
           <>
             <ProfileCover project={project} />
-            {!addNote?
-              <Grid container>
-                <Grid item s={3}>
-                  <Button onClick={() => setAddNote(true)}>
-                    Add Note
-                  </Button>
-                </Grid>
-              </Grid>
+            {!editNote?
+                <>
+                    <ReactMarkdown remarkPlugins={[remarkGfm, remarkBreaks]}>
+                        {project.notes}
+                    </ReactMarkdown>
+                    <Button onClick={editNotes} variant="outlined">Edit</Button>
+                </>
             :
-              <AddNoteForm
-                onSubmit={submitNote}
-                onCancel={() => setAddNote(false)}
-                />
+                <>
+                    <TextField
+                        multiline
+                        fullWidth
+                        minRows={4}
+                        placeholder="Project Notes"
+                        value={notes}
+                        onChange={(e) => setNotes(e.target.value)}
+                        sx={{
+                        '& fieldset': {
+                            borderWidth: `1px !important`,
+                            borderColor: (theme) => `${theme.palette.grey[500_32]} !important`,
+                        },
+                        }}
+                    />
+                    <Button onClick={saveNotes} variant="outlined">Save</Button>
+                </>
             }
           </>
           :
